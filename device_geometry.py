cd491e3ea15de9a331dfa4e258556d900a6a604f
Stephen More <stephen.w.more@gmail.com>
2024-07-26 09:25:59 -0400

    added comments

--- device_geometry.py
+++ device_geometry.py
@@ -2,10 +2,10 @@
 >> A Predictive 3-D Source/Drain Resistance Compact Model and the Impact on 7 nm and Scaled FinFets<<, 2020, with interpolation for 4nm. 16nm is taken from PTM HP.
 """
 
-node_names = [16, 7, 5, 4, 3]
-GP = [64, 56, 48, 44, 41]
-FP = [40, 30, 28, 24, 22]
-GL = [20, 18, 16, 15, 14]
-FH = [26, 35, 45, 50, 55]
-FW = [12, 6.5, 6, 5.5, 5.5]
-vdd = [0.85, 0.75, 0.7, 0.65, 0.65]
+node_names = [16, 7, 5, 4, 3]       # Process Node
+GP = [64, 56, 48, 44, 41]           # Gate pitch (nm)
+FP = [40, 30, 28, 24, 22]           # Fin pitch (nm)
+GL = [20, 18, 16, 15, 14]           # Gate length (nm)
+FH = [26, 35, 45, 50, 55]           # Fin height (nm) 
+FW = [12, 6.5, 6, 5.5, 5.5]         # Fin width (nm) 
+vdd = [0.85, 0.75, 0.7, 0.65, 0.65] # Vdd (V)
